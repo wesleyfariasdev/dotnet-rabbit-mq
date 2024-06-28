b97e0484f1bf345eb8d98691714f4b0a8d43c1b1
wesleysousukee@gmail.com <wesleysousukee@gmail.com>
2024-06-28 02:30:13 -0300

    configuração RabbitMQ e MassTransit

--- DotnetRabbitMQ/Extensions/AppExtensions.cs
+++ DotnetRabbitMQ/Extensions/AppExtensions.cs
@@ -1,10 +1,27 @@
-﻿namespace DotnetRabbitMQ.Extensions
+﻿using MassTransit;
+using MassTransit.Logging;
+using Microsoft.Extensions.Options;
+
+namespace DotnetRabbitMQ.Extensions
 {
     internal static class AppExtensions
     {
         public static void AddRabbitMQService(this IServiceCollection services)
         {
+            services.AddMassTransit(busConfigurator =>
+            {
+                busConfigurator.UsingRabbitMq((ctx, cfg) =>
+                 {
+                     cfg.Host(new Uri("amqp://172.17.0.2:5672"), host =>
+                     {
+                         host.Username("guest");
+                         host.Password("guest");
+                        
+                     });
 
+                     cfg.ConfigureEndpoints(ctx);
+                 });
+            });
         }
     }
 }
--- DotnetRabbitMQ/Program.cs
+++ DotnetRabbitMQ/Program.cs
@@ -1,11 +1,17 @@
+using DotnetRabbitMQ.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddRabbitMQService();
+
+builder.Services.AddControllers();
+
 
 var app = builder.Build();
 
